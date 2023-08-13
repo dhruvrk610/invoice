60e2e6a5c5a402bcc6745e15e06d825bf872fe66
dhruvrk610 <dhruvrk610@gmail.com>
2023-08-13 21:52:58 +0530

    c

--- src/components/TotalSection.js
+++ src/components/TotalSection.js
@@ -5,14 +5,14 @@ const TotalSection = (props) => {
     <div className='total-section'>
             <div className='container'>
               <div>
-                <h4>Total Amount: {isNaN(props.formattedTotalAmount) ? 'Plz Enter Values...' : props.formattedTotalAmount}  <span dangerouslySetInnerHTML={{ __html: props.currency }} /></h4>
+                <h4>Total Amount: {isNaN(props.formattedTotalAmount) ? '0.00' : props.formattedTotalAmount}  <span dangerouslySetInnerHTML={{ __html: props.currency }} /></h4>
               </div>
               <div>
-                <h4>Discount: ({props.discountRate}%): {isNaN(props.formatteddiscountAmount) ? 'Plz Enter Values...' : props.formatteddiscountAmount} <span dangerouslySetInnerHTML={{ __html: props.currency }} /></h4>
+                <h4>Discount: ({props.discountRate}%): {isNaN(props.formatteddiscountAmount) ? '0.00' : props.formatteddiscountAmount} <span dangerouslySetInnerHTML={{ __html: props.currency }} /></h4>
               </div>
               <hr />
               <div>
-                <h4>Grand Total: {isNaN(props.formattedGrandTotal) ? 'Plz Enter Values...' : props.formattedGrandTotal}  <span dangerouslySetInnerHTML={{ __html: props.currency }} /></h4>
+                <h4>Grand Total: {isNaN(props.formattedGrandTotal) ? '0.00' : props.formattedGrandTotal}  <span dangerouslySetInnerHTML={{ __html: props.currency }} /></h4>
               </div>
               <hr />
             </div>
